8aacc3b0205b78a3379a5e86e2717e949131066b
arifusq <dariaaa_ef812@mail.ru>
2025-10-15 11:51:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <img src="src\assets\lily-png-46482.png" className="logo" alt="Картинка" />
+      </div>
+      <h1>Ефимова Дарья 426-ВЕБ</h1>
+    </>
+  )
+}
+
+export default App
